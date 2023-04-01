9cec0677f2aec5d2cfc2ebf27383933306192cef
Thai Tinh Tran <43088138+thisisthaitinh@users.noreply.github.com>
2023-04-01 16:17:27 +0700

    Update lab3_fileopening.c

--- Lab3_struct/lab3_fileopening.c
+++ Lab3_struct/lab3_fileopening.c
@@ -1,167 +1,126 @@
 // library declaration
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define MAX 100
 
-// SinhVien
+int n, i;
+
+// StudentsName
 typedef struct
 {
-     char name[MAX];
-     int number;
-     float diem_tk, diem_gk, diem_ck, diem_th, diem_tb;
-} SinhVien;
+     char ten_ho[30], ten[30];
+} StudentsName;
 
-// variable
-int n, i;
-SinhVien a[MAX];
-
-// FailedStudents
-void FailedStudents(SinhVien *a, int n)
+// StudentsDayOfBirth
+typedef struct
 {
-     printf("\nSTT\tHo ten\t\tMa so sinh vien\tDiem trung binh\n");
-     for (i = 0; i < n; ++i)
-     {
-          if (a[i].diem_tb < 4.0)
-          {
-               printf("%d\t%s\t%d\t%.2f", i + 1, a[i].name, a[i].number, a[i].diem_tb);
-               printf("\n");
-          }
-     }
-}
+     char ngaySinh[20], noiSinh[40];
+} StudentsDayOfBirth;
 
-// TextDisplay
-void TextDisplay(SinhVien *a, int n)
+// StudentsPoints
+typedef struct
 {
-     printf("\n");
-     for (i = 0; i < n; ++i)
-     {
-          printf("Ho ten: %s\n", a[i].name);
-          printf("Ma so sinh vien: %d\n", a[i].number);
-          printf("Diem thuong ky: %.2f\n", a[i].diem_tk); // or a[i].diem.diem_tk;
-          printf("Diem giua ky: %.2f\n", a[i].diem_gk);
-          printf("Diem cuoi ky: %.2f\n", a[i].diem_ck);
-          printf("Diem thuc hanh: %.2f\n", a[i].diem_th);
-          printf("Diem trung binh: %.2f\n", a[i].diem_tb);
-     }
-}
+     int diemWin, diemExcel, diemWord, tongDiem;
+} StudentsPoints;
 
-// StatusDisplay
-const char *StatusDisplay(SinhVien a[])
+// StudentsManagement
+typedef struct
 {
-     char *failStatus = "Hoc lai", *successStatus = "Dat";
-     if (a[i].diem_tb < 4.0)
-     {
-          return failStatus;
-     }
-     else
-          return successStatus;
-}
+     StudentsName tenSinhVien;
+     StudentsDayOfBirth SinhVien;
+     StudentsPoints diemSinhVien;
+} StudentsManagement;
 
-// compare
-int compare(const void *s1, const void *s2)
+StudentsManagement svien[MAX];
+
+// NameAndBirth
+void nhapTTSV(StudentsName *tenSinhVien, StudentsDayOfBirth *SinhVien, StudentsPoints *diemSinhVien)
 {
-     const SinhVien *diem1 = s1;
-     const SinhVien *diem2 = s2;
-     if (diem1->diem_tb < diem2->diem_tb)
-     {
-          return 1;
-     }
-     else if (diem1->diem_tb > diem2->diem_tb)
-     {
-          return -1;
-     }
-     else
-          return 0;
-}
-
-// TableDisplay
-void TableDisplay(SinhVien *a, int n)
-{
-     FILE *file = fopen("output.txt", "w");
-     if (file == NULL)
-     {
-          printf("Khong the mo file!\n");
-          exit(1);
-     }
-     freopen("output.txt", "w", stdout); // redirect stdout to file
-     fprintf(file, "%s\t %s\t\t %s\t\t %s\t %s\t %s\t %s\t %s\t %s\n", "STT", "Ho ten", "MSSV", "TK", "GK", "CK", "TH", "DTB", "Ghi chu");
-     for (i = 0; i < n; ++i)
-     {
-          fprintf(file, "%d\t %s\t %d\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t %s\n", i + 1, a[i].name, a[i].number, a[i].diem_tk, a[i].diem_gk, a[i].diem_ck, a[i].diem_th, a[i].diem_tb, StatusDisplay(a));
-     }
-     // close file
-     fclose(file);
-
-     // open file in notepad
-     system("notepad.exe output.txt");
-}
-
-// FindStudent
-void FindStudent(SinhVien *a, int n)
-{
-     char sv[MAX];
-     int i, count = 0;
-     printf("Nhap ten sinh vien can tim: ");
-     gets(sv);
-     for (i = 0; i < n; ++i)
-     {
-          if (strcmp(sv, a[i].name) == 0)
-          {
-               TextDisplay(a, n);
-               count = 1;
-          }
-     }
-     if (count == 0)
-     {
-          printf("Khong the tim thay sinh vien %s!", sv);
-     }
-     fflush(stdin);
-}
-
-// ThongTinSinhVien
-void StudentInformation(SinhVien *a, int n)
-{
-     do
-     {
-          printf("Nhap thong tin cho bao nhieu sinh vien?: ");
-          scanf("%d", &n);
-          if (n <= 0)
-          {
-               printf("\nVui long nhap lai!\n\n");
-          }
-     } while (n <= 0);
+     printf("Ban muon nhap thong tin cho bao nhieu sinh vien?: ");
+     scanf_s("%d", &n);
      for (i = 0; i < n; ++i)
      {
           fflush(stdin);
-          printf("\nSinh vien thu %d: \n", i + 1);
-          printf("\n\tHo ten: ");
-          gets(a[i].name);
-          printf("\n\tMa so sinh vien: ");
-          scanf("%d", &a[i].number);
-          printf("\n\tNhap diem thuong ky, giua ky, cuoi ky va diem thuc hanh: ");
-          scanf("%f %f %f %f", &a[i].diem_tk, &a[i].diem_gk, &a[i].diem_ck, &a[i].diem_th);
-          a[i].diem_tb = (((((2 * a[i].diem_tk) + (3 * a[i].diem_gk) + (5 * a[i].diem_ck)) / 10) * 2 + a[i].diem_th)) / 3.0;
+          printf("\nHoc vien thu %d:\n", i + 1);
+
+          printf("\n\t- Nhap ho, ten dem: ");
+          gets(svien[i].tenSinhVien.ten_ho);
+
+          printf("\n\t- Nhap ten: ");
+          gets(svien[i].tenSinhVien.ten);
+
+          printf("\n\t- Nhap ngay sinh: ");
+          gets(svien[i].SinhVien.ngaySinh);
+
+          printf("\n\t- Nhap noi sinh: ");
+          gets(svien[i].SinhVien.noiSinh);
+
+          printf("\n\t- Nhap diem Windows: ");
+          scanf("%d", &svien[i].diemSinhVien.diemWin);
+
+          printf("\n\t- Nhap diem Excel: ");
+          scanf("%d", &svien[i].diemSinhVien.diemExcel);
+
+          printf("\n\t- Nhap diem Word: ");
+          scanf("%d", &svien[i].diemSinhVien.diemWord);
+
+          // finalPoint
+          svien[i].diemSinhVien.tongDiem = svien[i].diemSinhVien.diemWin + svien[i].diemSinhVien.diemExcel + svien[i].diemSinhVien.diemWord;
      }
-     qsort(a, n, sizeof(SinhVien), compare);
-     TableDisplay(a, n);
-     free(a);
 }
 
-// Main program
+// XepLoaiSinhVien
+const char *StatusDisplay(StudentsManagement svien[])
+{
+     char *excellent = "Gioi", *good = "Kha", *average = "Trung binh", *bad = "Kem";
+     if (svien[i].diemSinhVien.tongDiem >= 24)
+     {
+          return excellent;
+     }
+     if (18 <= svien[i].diemSinhVien.tongDiem < 24)
+     {
+          return good;
+     }
+    if (15 <= svien[i].diemSinhVien.tongDiem < 18)
+     {
+          return average;
+     }
+     if (svien[i].diemSinhVien.tongDiem < 15)
+     {
+          return bad;
+     }
+}
+
+// StudentsInfo
+StudentsManagement StudentsInfo()
+{
+	StudentsManagement svien;
+     nhapTTSV(&svien.tenSinhVien, &svien.SinhVien, &svien.diemSinhVien);
+}
+
+// StudentsDisplay
+StudentsManagement StudentsDisplay()
+{
+	FILE *file = fopen("display.txt", "w");
+	if (file == NULL) {
+		printf("Khong the mo file!");
+	}
+	freopen("display.txt", "w", stdout);
+    fprintf(file, "%s\t %s\t %s\t %s\t %s\t %s\n", "STT", "Ho va ten", "Ngay sinh", "Noi sinh", "DTB", "Ghi chu");
+     for (i = 0; i < n; ++i)
+     {
+          fprintf(file, "%d\t %s %s\t %s\t %s\t %d\t %s\n", i + 1, svien[i].tenSinhVien.ten_ho, svien[i].tenSinhVien.ten, svien[i].SinhVien.ngaySinh, svien[i].SinhVien.noiSinh, svien[i].diemSinhVien.tongDiem, StatusDisplay(svien));
+     }
+    // close file
+    fclose(file);
+    
+    // open file in notepad
+    system("notepad.exe display.txt");
+}
+
 int main()
 {
      system("cls");
-     // do
-     // {
-     //      StudentInformation(x, n);
-     //      printf("\nBan co muon thoat khoi chuong trinh k? (0 for no, 1 for yes): ");
-     //      scanf("%d", &choice);
-     //      if (choice == 1)
-     //      {
-     //           printf("\nThoat chuong trinh thanh cong!");
-     //      }
-     // } while (choice == 0);
-     StudentInformation(malloc(n * sizeof(SinhVien)), n);
-     return 0;
+     StudentsInfo();
+     StudentsDisplay();
 }
